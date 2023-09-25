031a99a6655fb93d4168ae8a1e68a46b067f3c39
Budaházi Bence <budahazi.bence@petrik.hu>
2023-09-25 18:07:39 +0200

    Számítás elvégzése, eredmény megjelenítése

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 import './style.css';
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    const szamitGomb = document.getElementById("szamit");
+    szamitGomb.addEventListener("click", teruletEsKeruletSzamit);
+}
+
+function teruletEsKeruletSzamit() {
+    const a = parseFloat(document.getElementById("aOldal").value);
+    const b = parseFloat(document.getElementById("bOldal").value);
+    const c = parseFloat(document.getElementById("cOldal").value);
+    const eredmeny = document.getElementById("eredmeny");
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        eredmeny.textContent = "Hibás adatok! A háromszög nem szerkeszthető";
+    }
+    else {
+        const haromszog = {
+          a: a,
+          b: b,
+          c: c,
+          kerulet: function () {
+            return this.a + this.b + this.c;
+          },
+          terulet: function () {
+            const s = this.kerulet() / 2;
+            return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+          },
+          keruletEsTeruletSzoveges: function() {
+            return `Kerület: ${this.kerulet()} - Terület ${this.terulet()}`;
+          }
+        };
+        console.log(haromszog);
+        eredmeny.textContent = haromszog.keruletEsTeruletSzoveges();
+    }
+}
